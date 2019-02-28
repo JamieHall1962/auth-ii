83a24a468fc03ae8254793642bd08f9577b91faa
Jamie Hall <hall.jamie@gmail.com>
2019-02-28 15:54:33 -0500

    React app structure in place

--- users/router.js
+++ users/router.js
@@ -50,6 +50,8 @@ function restricted(req, res, next) {
   }
 }
 
+
+
 // Design and build the following endpoints.
 
 // | POST   | /api/register | Creates a `user` using the information sent inside the `body` of the request. **Hash the password** before saving the user to the database.
@@ -86,36 +88,32 @@ router.post("/register", (req, res) => {
 // | POST   | /api/login    | Use the credentials sent inside the `body` to authenticate the user. On successful login, create a new JWT with the user id as the subject and send it back to the client. If login fails, respond with the correct status code and the message: 'You shall not pass!' |
 
 router.post("/login", (req, res) => {
-    const { username, password } = req.body;
-      
-    db("users")
-      .where({ username: username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          const token = generateToken(user);
-  
-          res.status(200).json({ token, message: `Welcome ${username}` });
-        } else {
-          res.status(401).json({ message: "You shall not pass!" });
-        }
-      })
-      .catch(err => res.status(500).send(err));
-  });
+  const { username, password } = req.body;
 
+  db("users")
+    .where({ username: username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
 
-
+        res.status(200).json({ token, message: `Welcome ${username}` });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => res.status(500).send(err));
+});
 
 // | GET    | /api/users    | If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in respond with the correct status code and the message: 'You shall not pass!'. Use this endpoint to verify that the password is hashed before it is saved.
 
 router.get("/users", restricted, (req, res) => {
-    db("users")
-      .select("id", "username", "password", "department")
-      .then(users => {
-        res.json(users);
-      })
-      .catch(err => res.send(err));
-  });
-
+  db("users")
+    .select("id", "username", "password", "department")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
 
 module.exports = router;
